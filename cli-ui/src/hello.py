00a572ba524e1ad8065b7fb2da3c9f2056ac356d
SergeyLim <limsergeystudy@gmail.com>
2025-04-26 21:25:51 +0200

    chore: restructure project with new file layout and Docker setup

---
+++ cli-ui/src/hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+
+
+def main():
+    
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
+        print(f"Привет, {name}!")
+    else:
+        print("Привет, мир!")
+
+
+if __name__ == "__main__":
+    main()
